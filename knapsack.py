e0523dec2aaf9067fb4595bcedcd76c592b9313f
biraj <khatiwadabizzz@gmail.com>
2021-08-24 23:05:52 -0700

    successfully populated the table

--- knapsack.py
+++ knapsack.py
@@ -1,10 +1,26 @@
-vals = [1,4,5,7]
-wts = [1,3,4,5]
-capacity = 5
+import numpy as np
+
+vals = [0,2,2,4,5,3]
+wts = [0,3,1,3,4,2]
+capacity = 7
 
 
 #creating the table
+width, height = capacity + 1, len(vals)
+table = [[0 for i in range(width)] for j in range(height)]
 
-#values, weights, capacity
-def knapsack(v, w, C):
 
+
+#populate the table 
+for row in range(height):
+    for column in range(width):
+        if(wts[row]<= column):
+            # print(row, column)
+            table[row][column] = max(vals[row]+table[row-1][column-wts[row]], table[row-1][column])
+
+        else:
+            table[row][column] = table[row-1][column]
+    
+#to print the table like table
+list_as_array = np.array(table)
+print(list_as_array)
